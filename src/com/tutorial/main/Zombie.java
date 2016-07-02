37e2c3b3b50599f37ce1cd6190b92595af4b8c56
Ethan Adams <ethanadamsm@gmail.com>
2016-07-02 01:08:37 -0600

    Zombie render, tick

--- src/com/tutorial/main/Zombie.java
+++ src/com/tutorial/main/Zombie.java
@@ -1,5 +1,22 @@
 package com.tutorial.main;
 
-public class Zombie {
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class Zombie extends GameObject{
+
+	public Zombie(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	public void tick() {
+		x += velX;
+		y += velY;
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(x, y, 32, 32);
+	}
+	
 }
